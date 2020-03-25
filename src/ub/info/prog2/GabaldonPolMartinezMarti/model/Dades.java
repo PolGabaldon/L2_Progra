be9c7aec95df6b4dc83a3bf93e60591f8289f62b
polg24 <polg24@lenovo>
2020-03-25 12:24:44 +0100

    Dades

--- src/ub/info/prog2/GabaldonPolMartinezMarti/model/Dades.java
+++ src/ub/info/prog2/GabaldonPolMartinezMarti/model/Dades.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ub.info.prog2.GabaldonPolMartinezMarti.model;
 
-/**
- *
- * @author polg24
- */
-public class Dades {
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import ub.info.prog2.GabaldonPolMartinezMarti.controlador.Motor;
+import ub.info.prog2.utils.ReproException;
+
+
+public class Dades implements Serializable{
+    private RepositoriFitxersMultimedia repositori;
+    private ArrayList<PortafoliFitxersMultimedia> portafolis;
     
+    public Dades(){
+        repositori = new RepositoriFitxersMultimedia();
+        portafolis = new ArrayList<>();
+    }
+    
+    public void addAudio(String camiFitxerAudio, String camiFitxerImatge, String autor, String codec, int kbps) throws ReproException{
+        File fitxerImatge = new File(camiFitxerImatge);
+        Motor motor = new Motor();
+        Audio audio = new Audio(camiFitxerAudio, fitxerImatge, autor, codec, kbps, motor);
+        repositori.addFitxer(audio);
+    }
+    
+    public void addImatge(String cami, String autor, String codec, int pixelsAlcada, int pixelsAmplada) throws ReproException{
+        Motor motor = new Motor();
+        Imatge imatge = new Imatge(cami, autor, codec, pixelsAlcada, pixelsAmplada, motor);
+        repositori.addFitxer(imatge);
+    }
 }
