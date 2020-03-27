ef9899583267dc9220c4a067e483bede00f5ed23
marti <marti@marti-HP-Pavilion-Laptop-14-ce2xxx>
2020-03-27 03:55:05 +0100

    reproductor ub2 acabat

--- src/ub/info/prog2/GabaldonPolMartinezMarti/vista/ReproductorUB2.java
+++ src/ub/info/prog2/GabaldonPolMartinezMarti/vista/ReproductorUB2.java
@@ -2,6 +2,9 @@ package ub.info.prog2.GabaldonPolMartinezMarti.vista;
 
 import java.util.Scanner;
 import ub.info.prog2.utils.Menu;
+import ub.info.prog2.GabaldonPolMartinezMarti.controlador.Controlador;
+import ub.info.prog2.utils.ReproException;
+
 
 
 /**
@@ -34,16 +37,19 @@ public class ReproductorUB2 {
     private Menu<ReproductorUB2.OpcionsMenuPrincipal> menu;
     private Menu<ReproductorUB2.OpcionsSubmenu1> subMenu1;
     private Menu<ReproductorUB2.OpcionsSubmenu2> subMenu2;
+    private Controlador controlador;
     
-    
-        public ReproductorUB2(){
+    public ReproductorUB2(){
         menu = new Menu<OpcionsMenuPrincipal>("Menu Principal",ReproductorUB2.OpcionsMenuPrincipal.values());
         subMenu1 = new Menu<OpcionsSubmenu1>("Menu Secundari",ReproductorUB2.OpcionsSubmenu1.values());
         subMenu2 = new Menu<OpcionsSubmenu2>("Subemnú",ReproductorUB2.OpcionsSubmenu2.values());
+        controlador = new Controlador();
 
     }
             
     public void gestioReproductorUB(Scanner sc){
+        
+        String cami;
         // Assignem la descripció de les opcions
         menu.setDescripcions(descMenuPrincipal);
         
@@ -61,13 +67,28 @@ public class ReproductorUB2 {
             switch(opcio) {
                 case GESTIO_FITXERS:
                     gestioMenu2(sc);
-
                     break;
                    
                 case GUARDAR_DADES:
+                    System.out.println("Introdueix el camí del fitxer de disc on guardar les dades: ");
+                    cami = sc.nextLine();
+                    try{
+                    controlador.saveDades(cami);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                        
+                    }                    
                     break;
                     
                 case RECUPERAR_DADES:
+                    System.out.println("Introdueix el camí del fitxer de disc d'on recuperar les dades: ");
+                    cami = sc.nextLine();
+                    try{
+                    controlador.loadDades(cami);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                        
+                    }                    
                     break;
                 
                 case SORTIR:
@@ -85,6 +106,8 @@ public class ReproductorUB2 {
      * @param sc Objecte de tipus Scanner que permet accedir al teclat
      */
     public void gestioMenu2(Scanner sc){
+        String nom;
+        int opcio1, fitxer;
 
         // Assignem la descripciÃ³ de les opcions
         subMenu1.setDescripcions(descMenu2);
@@ -100,22 +123,118 @@ public class ReproductorUB2 {
             // Fem les accions necessàries
             switch(opcio) {
                 case CREAR_PORTAFOLI:
+                    System.out.println("Introdueix el nom del nou portafoli: ");
+                    nom = sc.nextLine();
+                    try{
+                        controlador.addPortafoli(nom);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                        
+                        
+                    }
+                    
                     break;
                    
                 case MOSTRAR_PORTAFOLIS:
+                    controlador.showPortafolis();
                     break;
                     
                 case ELIMINAR_PORTAFOLI:
+                    System.out.println("Introdueix el nom del portafolis que vols eliminar");
+                    nom = sc.nextLine();
+                    try{
+                        controlador.removePortafoli(nom);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                        
+                        
+                    }
                     break;
                     
                 case AFEGIR_FITXER:
-                    gestioMenu3(sc);
+                    opcio1 = 0;
+                    while(opcio1 !=1 && opcio1 !=2 && opcio1 != 3){
+                        System.out.println("Pitja 1 per afegir un fitxer a un portafolis\n"
+                                + "Pitja 2 per afegir un fitxer al repositori\n"
+                                + "Pitja 3 per sortir");
+                        opcio1 = sc.nextInt();
+                                        
+                    }
+                    if(opcio1 == 1){
+                        controlador.showPortafolis();
+                        System.out.println("A quin portafolis vols afegir el fitxer?");
+                        nom = sc.nextLine();
+                        controlador.showRepositori();
+                        System.out.println("Quin dels fitxers del repositori vols afegir?");
+                        fitxer = sc.nextInt();
+                        try{
+                        controlador.addFitxer(nom, fitxer);
+                        }
+                        catch(ReproException e){
+                            System.out.println(e.getCause());                                                    
+                        }
+                    }
+                    else if(opcio1 == 2){
+                        gestioMenu3(sc);
+                    }
                     break;
                     
                 case MOSTRAR_FITXERS:
+                    opcio1 = 0;
+                    while(opcio1 != 1 && opcio1 !=2 && opcio1 !=3){
+                    System.out.println("Pitja 1 per mostrar el repositori\n"
+                            + "Pitja 2 per mostrar un portafoli\n"
+                            + "Pitja 3 per sortir");
+                    opcio1 = sc.nextInt();
+                    }
+                    if(opcio1 == 1){
+                        controlador.showRepositori();
+                    }
+                    else if(opcio1 ==2){
+                        controlador.showPortafolis();
+                        System.out.println("Quin portafoli vols mostrar?");
+                        nom = sc.nextLine();
+                        try{
+                            controlador.showPortafoli(nom);
+                        }
+                        catch(ReproException e){
+                            System.out.println(e.getCause());                                                                                
+                        }
+                    }
                     break;
                     
                 case ELIMINAR_FITXER:
+                    opcio1 = 0;
+                    while(opcio1 != 1 && opcio1 !=2 && opcio1 !=3){
+                    System.out.println("Pitja 1 per eliminar un fitxer del repositori\n"
+                            + "Pitja 2 per eliminar un fitxer d'un portafoli\n"
+                            + "Pitja 3 per sortir");
+                    opcio1 = sc.nextInt();
+                    }
+                    if(opcio1 == 1){
+                        controlador.showRepositori();
+                        System.out.print("Digues el número del fitxer que vols eliminar: ");
+                        fitxer = sc.nextInt();
+                        try{
+                            controlador.removeFitxer(fitxer);
+                        }
+                        catch(ReproException e){
+                            System.out.println(e.getCause());
+                        }
+                    }
+                    else if(opcio1 ==2){
+                        controlador.showPortafolis();
+                        System.out.println("De quin portafoli vols eliminar el fitxer?");
+                        nom = sc.nextLine();
+                        System.out.println("Digues el número del fitxer que vols eliminar:");
+                        fitxer = sc.nextInt();
+                        try{
+                            controlador.removeFitxer(nom, fitxer);
+                        }
+                        catch(ReproException e){
+                            System.out.println(e.getCause());                                                                                
+                        }
+                    }
                     break;
                     
                 case MENU_ANTERIOR:
@@ -127,6 +246,8 @@ public class ReproductorUB2 {
     }
     
     public void gestioMenu3(Scanner sc){
+        String cami1, cami2, nom, codec;
+        int i,j;
         
         // Assignem la descripciÃ³ de les opcions
         subMenu2.setDescripcions(descMenu3);
@@ -142,9 +263,46 @@ public class ReproductorUB2 {
             // Fem les accions necessàries
             switch(opcio) {
                 case AFEGIR_AUDIO:
+                    System.out.println("Introdueix el camí del fitxer:");
+                    cami1 = sc.nextLine();
+                    System.out.println("Introdueix el camí del fitxer d'imatge:");
+                    cami2 = sc.nextLine();
+                    System.out.println("Introdueix el nom de l'autor:");
+                    nom = sc.nextLine();
+                    System.out.println("Introdueix el codec:");
+                    codec = sc.nextLine();
+                    System.out.println("Introdueix els kbps:");
+                    i = sc.nextInt();
+                    
+                    try{
+                    controlador.addAudio(cami1, cami2, nom, codec, i);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                                                    
+                        
+                    }
                     break;
                    
                 case AFEGIR_IMATGE:
+                    System.out.println("Introdueix el camí del fitxer");
+                    cami1 = sc.nextLine();
+                    System.out.println("Introdueix el nom de l'autor:");
+                    nom = sc.nextLine();
+                    System.out.println("Introdueix el codec:");
+                    codec = sc.nextLine();
+                    System.out.println("Introdueix els píxels d'alçada");
+                    i = sc.nextInt();
+                    System.out.println("Introdueix els píxels d'amplada");
+                    j = sc.nextInt();
+
+                    try{
+                    controlador.addImatge(cami1, nom, codec, i, j);
+                    }
+                    catch(ReproException e){
+                        System.out.println(e.getCause());                                                    
+                        
+                    }
+                            
                     break;
 
                 case MENU_ANTERIOR:
